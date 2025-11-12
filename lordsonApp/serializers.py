b5da9fa72df23e08515886e5f5e38c70ea17b9e5
freakfit22 <freakfit22@gmail.com>
2025-11-13 03:04:48 +0530

    test10

--- lordsonApp/serializers.py
+++ lordsonApp/serializers.py
@@ -1,15 +1,11 @@
 from rest_framework import serializers
-from .models import Banner, Product
+from .models import Banner, Product, ProductImage
 
 
 class BannerSerializer(serializers.ModelSerializer):
     class Meta:
         model = Banner
         fields = '__all__'
-
-
-class ProductImage:
-    pass
 
 
 class ProductImageSerializer(serializers.ModelSerializer):
