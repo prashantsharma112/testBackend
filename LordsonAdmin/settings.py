ef308ad59f7151cdd4abad2020c07b7c7d0aa8ea
freakfit22 <freakfit22@gmail.com>
2025-11-13 02:12:20 +0530

    test10

--- LordsonAdmin/settings.py
+++ LordsonAdmin/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 import os
 import dj_database_url
-from dotenv import load_dotenv
 from pathlib import Path
 from dotenv import load_dotenv
 load_dotenv()
--- lordsonApp/urls.py
+++ lordsonApp/urls.py
@@ -1,13 +1,6 @@
-from django.urls import path, include
-from rest_framework.routers import DefaultRouter
-from .views import BannerViewSet, ProductViewSet, OrderViewSet  # 👈 also include ProductViewSet if you have it
-
-# Create a router and register your API endpoints
-router = DefaultRouter()
-router.register(r'banners', BannerViewSet, basename='banner')
-router.register(r'products', ProductViewSet, basename='product')
-router.register(r'orders', OrderViewSet)
+from django.urls import path
+from . import views
 
 urlpatterns = [
-    path('', include(router.urls)),
+    path("", views.home, name="home"),
 ]
